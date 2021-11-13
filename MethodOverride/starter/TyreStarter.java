735b8bc640d086169c0ddec4f5b8ced99a65b306
Pooja C <91308685+poojaxwork@users.noreply.github.com>
2021-11-14 02:07:23 +0530

    Update TyreStarter.java

--- MethodOverride/starter/TyreStarter.java
+++ MethodOverride/starter/TyreStarter.java
@@ -1,10 +1,19 @@
 package com.xwork.starter;
 
+import com.xwork.diode.Tyre;
+
 public class TyreStarter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Tyre ref = new Tyre();
+		System.out.println(ref.toString());
+		System.out.println(ref.hashCode());
+		Tyre ref1 = new Tyre();
+		ref.setBrand("MRF");
+		ref1.setBrand("CEAT");
+		ref.setPrice(400.6f);
+		ref1.setPrice(400.6f);
+		System.out.println(ref.equals(ref1));
 	}
 
 }
