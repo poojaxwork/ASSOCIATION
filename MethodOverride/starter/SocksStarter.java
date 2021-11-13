9aa1afe7d35fc11635287011502c4b65c0ae521a
Pooja C <91308685+poojaxwork@users.noreply.github.com>
2021-11-14 01:45:48 +0530

    Update SocksStarter.java

--- MethodOverride/starter/SocksStarter.java
+++ MethodOverride/starter/SocksStarter.java
@@ -1,8 +1,21 @@
 package com.xwork.starter;
 
+import com.xwork.constant.Material;
+import com.xwork.diode.Socks;
+
 public class SocksStarter {
 
 	public static void main(String[] args) {
+		Socks sock = new Socks();
+
+		System.out.println(sock.toString());
+		System.out.println(sock.hashCode());
+		Socks sock1 = new Socks();
+		sock.setPrice(100);
+		sock1.setPrice(100);
+		sock.setMaterial(Material.COTTON);
+		sock1.setMaterial(Material.COTTON);
+		System.out.println(sock.equals(sock1));
 
 	}
 
