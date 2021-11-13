a9a61f8d186fca0c9cd6994d1112be15e35176e4
Pooja C <91308685+poojaxwork@users.noreply.github.com>
2021-11-14 01:42:56 +0530

    Update NewsRunner.java

--- MethodOverride/starter/NewsRunner.java
+++ MethodOverride/starter/NewsRunner.java
@@ -1,9 +1,20 @@
+
 package com.xwork.starter;
+
+import com.xwork.diode.NewsPaper;
 
 public class NewsRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		NewsPaper news = new NewsPaper();
+		System.out.println(news.toString());
+		System.out.println(news.hashCode());
+		NewsPaper news1 = new NewsPaper();
+		news.setLang("Kannada");
+		news1.setLang("Kannad");
+		news.setName("VijayVani");
+		news1.setName("VijayVani");
+		System.out.println(news.equals(news1));
 
 	}
 
